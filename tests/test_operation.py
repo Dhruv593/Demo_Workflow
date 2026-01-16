a7a49c6cc03838096346077385648c2bc184ecb9
Dhruv Lad <dhruvlad4444@gmail.com>
2026-01-16 12:00:52 +0530

    multiplication and division

--- tests/test_operation.py
+++ tests/test_operation.py
@@ -1,4 +1,4 @@
-from src.math_operations import add, sub
+from src.math_operations import add, sub, mul, div
 
 def test_add():
     assert add(2,3) == 5
@@ -8,3 +8,16 @@ def test_sub():
         assert sub(2,3) == -1
         assert sub(5,5) == 0
         assert sub(10,3) == 7
+
+def test_mul():
+    assert mul(2,3) == 6
+    assert mul(5,5) == 25
+    assert mul(0,10) == 0   
+
+def test_div():
+    assert div(6,3) == 2
+    assert div(10,2) == 5
+    try:
+        div(5,0)
+    except ValueError as e:
+        assert str(e) == "Cannot divide by zero."
